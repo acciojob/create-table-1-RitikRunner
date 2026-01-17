8a3a79affd1b1ffb9f58590db93e327b164609c8
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-17 13:00:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+const sampleTable = document.getElementById("sampleTable")
+const insertion = document.getElementById("insertion")
+
 function insert_Row() {
     //Write your code here
-  
-  
+  const row = sampleTable.insertRow();
+
+	const cell1 = row.insertCell(0);
+	const cell2 = row.insertCell(1);
+
+	cell1.textContent = "New Cell1";
+	cell2.textContent = "New Cell2";
 }
+insertion.addEventListener("click", insert_Row)
