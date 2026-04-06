732396d5de02fe76e41b414e3fdef547c8d155a1
Ritik Sharma <ritiksharma10508@gmail.com>
2026-04-06 20:59:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const insertion = document.getElementById("insertion")
 
 function insert_Row() {
     //Write your code here
-    const row = sampleTable.insertRow(0);
+    const row = sampleTable.insertRow(-1);
 
 	const cell1 = row.insertCell(0);
 	const cell2 = row.insertCell(1);
@@ -11,4 +11,4 @@ function insert_Row() {
 	cell1.textContent = "New Cell1";
 	cell2.textContent = "New Cell2";
 }
-insertion.addEventListener("click", insert_Row)
+// insertion.addEventListener("click", insert_Row)
